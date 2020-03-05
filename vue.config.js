eb5afa59538c192c080ce63cb63919f53c20feeb
Julia Evans <julia@jvns.ca>
2020-03-05 10:16:55 -0500

    add pngs

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,30 @@
 module.exports = {
   pages: {
-    //index: {
-    //  // entry for the page
-    //  entry: 'src/main.ts',
-    //  // the source template
-    //  template: 'public/index.html',
-    //  // output as dist/index.html
-    //  filename: 'index.html',
-    //  // when using title option,
-    //  // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-    //  title: 'container flashcards',
-    //  // chunks to include on this page, by default includes
-    //  // extracted common chunks and vendor chunks.
-    //  chunks: ['chunk-vendors', 'chunk-common', 'index'],
-    //  path: "/",
-    //},
-    //"container-basics": {
-    //    entry: 'src/pages/container-basics.ts',
-    //    title: 'container flashcards',
-    //    filename: 'index.html',
-    //},
+    index: {
+      // entry for the page
+      entry: 'src/main.ts',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'container flashcards',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+    "container-basics": {
+        entry: 'src/pages/container-basics.ts',
+        title: 'container flashcards',
+        template: 'public/index.html',
+        filename: 'container-basics/index.html',
+    },
     "linux": {
         entry: 'src/pages/linux.ts',
         title: 'linux flashcards',
-        filename: 'index.html',
+        template: 'public/index.html',
+        filename: 'linux/index.html',
     },
   }
 }
