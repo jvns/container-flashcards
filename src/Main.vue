617c77e3b68bdc9353ce8083cf5c1a5eedb04363
Julia Evans <julia@jvns.ca>
2020-03-05 10:38:14 -0500

    add a homepage

--- src/Main.vue
+++ src/Main.vue
@@ -1,6 +1,14 @@
 <template>
-    <div id="app">
-        hi
+    <div id="app" class="px-16 py-8">
+        <h1 class="text-3xl text-center">flashcards!</h1>
+        <p class="py-4">
+        Here are the 2 sets of flashcards so far:
+        <br><br>
+        </p>
+        <p class="underline text-blue-800">
+        <a class="hover:text-blue-400" href="/container-basics">Container basics</a><br>
+        <a class="hover:text-blue-400" href="/linux">Linux</a>
+        </p>
     </div>
 </template>
 
