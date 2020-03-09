999caa559ea23d391ab4f3ee451059cc1fef08ae
Julia Evans <julia@jvns.ca>
2020-03-09 17:48:24 -0400

    increase resolution

--- public/cards/pngify.sh
+++ public/cards/pngify.sh
@@ -1,13 +1,13 @@
 for i in "$@"
 do
 if [[ $i == *"back"* ]]; then
-    convert -background none -density 200 $i \
+    convert -background none -density 400 $i \
         -channel rgba -alpha set \
         -fill '#d9a36e' -opaque none \
         -define png:exclude-chunk=time,date \
         -alpha off -negate ${i/.svg/.png}
 else
-    convert -background white -density 200 $i \
+    convert -background white -density 400 $i \
         -define png:exclude-chunk=time,date \
         ${i/.svg/.png}
 fi
